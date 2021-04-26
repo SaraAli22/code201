ab4167f2c2e6519bb4c96b8b8fcd321132b70844
SaraAli22 <sarasalem48@yahoo.com>
2021-04-27 02:12:13 +0300

    adding code201

---
+++ Js/app.js
@@ -0,0 +1,13 @@
+
+var username = prompt("Please Enter your name");
+alert ("Welcome"+username)
+let userage = Number(prompt('What is your Age ?'));
+var age = 15 ;
+if (userage >= age)
+    {
+      alert('Recommond you to choose adult books');
+
+    } else if(userage< age)
+    {
+      alert('Recommond you to choose Kids books'); 
+      }
