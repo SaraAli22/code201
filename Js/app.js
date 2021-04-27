b92ff540ab3a8ddd033729652bc94835e17e30a8
SaraAli22 <sarasalem48@yahoo.com>
2021-04-28 01:38:01 +0300

    Edit

--- Js/app.js
+++ Js/app.js
@@ -1,8 +1,8 @@
 
-var username = prompt("Please Enter your name");
+let username = prompt("Please Enter your name");
 alert ("Welcome"+username)
 let userage = Number(prompt('What is your Age ?'));
-var age = 15 ;
+let age = 15 ;
 if (userage >= age)
     {
       alert('Recommond you to choose adult books');
@@ -11,3 +11,8 @@ if (userage >= age)
     {
       alert('Recommond you to choose Kids books'); 
       }
+
+      let userlanguage = prompt('Do you Prefer English or Arabic Books ?');
+      let userFav = prompt('Do you Prefer Romantic or comedy Books ?');
+alert ("Thanks for your Time"+username)
+
